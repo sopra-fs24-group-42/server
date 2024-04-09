09b621110cb221d262fdbf31ae830e4b507d1d67
kuppolina <polina.kuptsova@uzh.ch>
2024-04-09 12:39:57 +0200

    closes #28 , #27 implemented http post methods for creating lobby and player

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/SetupController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/SetupController.java
@@ -1,11 +1,16 @@
 package ch.uzh.ifi.hase.soprafs24.controller;
 
+import ch.uzh.ifi.hase.soprafs24.entity.Player;
+import ch.uzh.ifi.hase.soprafs24.entity.Lobby;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.LobbyDTO;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.PlayerDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.PlayerPostDTO;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.LobbyPostDTO;
 import ch.uzh.ifi.hase.soprafs24.service.LobbyService;
+import ch.uzh.ifi.hase.soprafs24.rest.mapper.DTOMapper;
 import ch.uzh.ifi.hase.soprafs24.service.PlayerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 
 /**
  * User Controller
@@ -25,18 +30,31 @@ public class SetupController {
     this.lobbyService = lobbyService;
   }
 
-
   @PostMapping("/players")
-  @ResponseStatus(HttpStatus.OK)
+  @ResponseStatus(HttpStatus.CREATED)
   @ResponseBody
-  public void createPlayer() {
-    
+  public PlayerDTO createPlayer(@RequestBody PlayerPostDTO playerPostDTO) {
+      // convert API user to internal representation
+      Player playerInput = DTOMapper.INSTANCE.convertPlayerPostDTOtoEntity(playerPostDTO);
+
+      // create user
+      Player createdPlayer = playerService.createUser(playerInput);
+
+      // convert internal representation of user back to API
+      return DTOMapper.INSTANCE.convertEntityToPlayerDTO(createdPlayer);
   }
 
   @PostMapping("/lobbies")
   @ResponseStatus(HttpStatus.CREATED)
   @ResponseBody
-  public void createLobby(@RequestBody PlayerPostDTO userPostDTO) {
-    
+  public LobbyDTO createLobby(@RequestBody LobbyPostDTO lobbyPostDTO) {
+      // convert API lobby to internal representation
+      Lobby lobbyInput = DTOMapper.INSTANCE.convertLobbyPostDTOtoEntity(lobbyPostDTO);
+
+      // create lobby
+      Lobby createdLobby = lobbyService.createLobby(lobbyInput);
+
+      // convert internal representation of user back to API
+      return DTOMapper.INSTANCE.convertEntityToLobbyDTO(createdLobby);
   }
 }
--- src/main/java/ch/uzh/ifi/hase/soprafs24/service/LobbyService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/service/LobbyService.java
@@ -1,5 +1,47 @@
 package ch.uzh.ifi.hase.soprafs24.service;
 
+import ch.uzh.ifi.hase.soprafs24.constant.GameState;
+import ch.uzh.ifi.hase.soprafs24.entity.Lobby;
+import ch.uzh.ifi.hase.soprafs24.utils.LobbyCodeGenerator;
+import ch.uzh.ifi.hase.soprafs24.repository.LobbyRepository;
+import ch.uzh.ifi.hase.soprafs24.repository.PlayerRepository;
+import ch.uzh.ifi.hase.soprafs24.utils.GameSettings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Transactional
 public class LobbyService {
-    
+    private final Logger log = LoggerFactory.getLogger(LobbyService.class);
+
+    private final LobbyRepository lobbyRepository;
+
+    @Autowired
+    public LobbyService(@Qualifier("lobbyRepository") LobbyRepository lobbyRepository) {
+        this.lobbyRepository = lobbyRepository;
+    }
+
+    public Lobby createLobby(Lobby newLobby) {
+        String lobbyCode = LobbyCodeGenerator.generateLobbyCode();
+        newLobby.setLobbyCode(lobbyCode);
+
+        newLobby.setGameState(GameState.NIGHT);
+        newLobby.setGameSettings(new GameSettings());
+
+        newLobby = lobbyRepository.save(newLobby);
+        lobbyRepository.flush();
+
+        log.debug("Created Information for Lobby: {}", newLobby);
+        return newLobby;
+    }
+
 }
