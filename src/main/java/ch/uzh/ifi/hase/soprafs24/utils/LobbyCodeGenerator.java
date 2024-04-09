1aacec255a6e0d5292739db045e6e0d8e5e6d589
kuppolina <polina.kuptsova@uzh.ch>
2024-04-09 12:31:03 +0200

    fixes #25 implemented correct mapping java objects to db tables

--- src/main/java/ch/uzh/ifi/hase/soprafs24/utils/LobbyCodeGenerator.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/utils/LobbyCodeGenerator.java
@@ -2,18 +2,21 @@ package ch.uzh.ifi.hase.soprafs24.utils;
 
 import java.security.SecureRandom;
 
-// add the description of the class
 public class LobbyCodeGenerator {
-    private static final int Length = 5;
+    private static final int Length = 5; // code lenght
     private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    // object of the java class SecureRandom
+    // to generate cryptographically strong random number
     private static final SecureRandom random = new SecureRandom();
 
+    // logic for code generation
     public static String generateLobbyCode() {
-        StringBuilder code = new StringBuilder();
+        StringBuilder code = new StringBuilder(); // instance to construct the string
         for (int i = 0; i < Length; i++) {
-            int randomIndex = random.nextInt(CHARACTERS.length());
-            char randomChar = CHARACTERS.charAt(randomIndex);
-            code.append(randomChar);
+            int randomIndex = random.nextInt(CHARACTERS.length()); // selects random position
+            char randomChar = CHARACTERS.charAt(randomIndex); // gets the char at the position
+            code.append(randomChar); // append with string builder to create a code
         }
         return code.toString();
     }
