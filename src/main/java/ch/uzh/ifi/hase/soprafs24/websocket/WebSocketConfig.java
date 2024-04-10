c74b9ad4a922f046301e3437a753580b2abbc59c
Lukas Niedhart <lukas.niedhart@gmx.ch>
2024-04-10 18:57:37 +0200

    resolves #32

--- src/main/java/ch/uzh/ifi/hase/soprafs24/websocket/WebSocketConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/websocket/WebSocketConfig.java
@@ -12,9 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-       registry.addEndpoint("/game")
-                .withSockJS()
-                .setHeartbeatTime(10000);
+       registry.addEndpoint("/game").withSockJS();
     }
 
     @Override
