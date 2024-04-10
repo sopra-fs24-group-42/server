857f3e4799bef8f502e57c5b685f082516a6d6e1
kuppolina <polina.kuptsova@uzh.ch>
2024-04-10 18:07:27 +0200

    closes #26 created LobbyRepository

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/repository/LobbyRepository.java
@@ -0,0 +1,11 @@
+package ch.uzh.ifi.hase.soprafs24.repository;
+
+import ch.uzh.ifi.hase.soprafs24.entity.Lobby;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("lobbyRepository")
+public interface LobbyRepository extends JpaRepository<Lobby, Long> {
+    Lobby findByLobbyCode(String lobbyCode);
+    Lobby findByLobbyId(Long lobbyId);
+}
