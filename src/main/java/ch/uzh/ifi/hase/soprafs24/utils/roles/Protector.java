050694d7051629491ae09d0c4dc7a6a522f9b2a5
Lukas Niedhart <lukas.niedhart@gmx.ch>
2024-05-03 18:00:45 +0200

    added missing annotation

--- src/main/java/ch/uzh/ifi/hase/soprafs24/utils/roles/Protector.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/utils/roles/Protector.java
@@ -1,10 +1,12 @@
 package ch.uzh.ifi.hase.soprafs24.utils.roles;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import ch.uzh.ifi.hase.soprafs24.service.ServiceProvider;
 import ch.uzh.ifi.hase.soprafs24.utils.Role;
 
+@Component
 public class Protector extends Role {
     private ServiceProvider serviceProvider;
 
