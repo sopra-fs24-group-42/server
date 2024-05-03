31125f128c0bfd200ac8e0f1b1016131f6087540
kuppolina <polina.kuptsova@uzh.com>
2024-05-03 18:40:36 +0200

    test api

--- src/test/java/ch/uzh/ifi/hase/soprafs24/repository/PlayerRepositoryIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/repository/PlayerRepositoryIntegrationTest.java
@@ -128,33 +128,6 @@ public class PlayerRepositoryIntegrationTest {
 
     @Test
     void deleteByPlayerId_success() {
-//        //Specify base URI
-//        RestAssured.baseURI="https://reqres.in/api/users?page=2";
-//
-//        //Request object
-//        RequestSpecification httpRequest=RestAssured.given();
-//
-//
-//        //print response in console window//Request payload sending along with post request
-//        JSONObject requestParams=new JSONObject();
-//
-//        requestParams.put("id",14);
-//
-//        httpRequest.header("Content-Type","application/json");
-//
-//        httpRequest.body(requestParams.toJSONString()); // attach above data to the request
-//
-//        //Response object
-//        Response response=httpRequest.request(Method.DELETE);
-//
-//        String responseBody=response.getBody().asString();
-//        System.out.println("Response Body is:" +responseBody);
-//
-//        //status code validation
-//        int statusCode=response.getStatusCode();
-//        System.out.println("Status code is: "+statusCode);
-//        Assert.assertEquals(statusCode, 204);
-
 
     }
 }
