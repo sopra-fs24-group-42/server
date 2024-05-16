683e0a6adcf36379c6126a8c6608ba908ea7eabc
kuppolina <polina.kuptsova@uzh.com>
2024-05-16 07:55:11 +0200

    added tests for reset lobby; added templates for other missing tests;

--- src/test/java/ch/uzh/ifi/hase/soprafs24/service/LobbyServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/service/LobbyServiceTest.java
@@ -1,11 +1,13 @@
 package ch.uzh.ifi.hase.soprafs24.service;
 
 import ch.uzh.ifi.hase.soprafs24.entity.Lobby;
+import ch.uzh.ifi.hase.soprafs24.entity.Player;
 import ch.uzh.ifi.hase.soprafs24.constant.GameState;
 import ch.uzh.ifi.hase.soprafs24.constant.WinnerSide;
 import ch.uzh.ifi.hase.soprafs24.utils.GameSettings;
 import ch.uzh.ifi.hase.soprafs24.repository.RepositoryProvider;
 import ch.uzh.ifi.hase.soprafs24.repository.LobbyRepository;
+import ch.uzh.ifi.hase.soprafs24.repository.PlayerRepository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
@@ -15,12 +17,17 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.server.ResponseStatusException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class LobbyServiceTest {
 
@@ -29,6 +36,9 @@ public class LobbyServiceTest {
 
     @Mock
     private LobbyRepository lobbyRepository;
+
+    @Mock
+    private PlayerRepository playerRepository;
 
     @InjectMocks
     private LobbyService lobbyService;
@@ -47,8 +57,10 @@ public class LobbyServiceTest {
         Mockito.when(repositoryProvider.getLobbyRepository()).thenReturn(lobbyRepository);
         Lobby lobby = new Lobby();
         lobby.setLobbyId(1L);
+        lobby.setLobbyCode("HHHHH");
         lobby.setHostName("testPlayer");
         lobby.setMinNumOfPlayers(4);
+        lobby.setNumberOfPlayers(4);
         lobby.setGameSettings(new GameSettings());
         this.testLobby = lobby;
     }
@@ -68,6 +80,7 @@ public class LobbyServiceTest {
         Mockito.verify(lobbyRepository, times(1)).save(testLobby);
     }
 
+    // fix reading the logger
     @Test
     void updateGameSettings_NotEnoughRoles() {
         int minNumOfPlayers = 4;
@@ -82,12 +95,67 @@ public class LobbyServiceTest {
         // '{}' < '{}'. Roles were not updated", minNumOfPlayers - 1, minNumOfPlayers);
     }
 
+    // fix the test
     // @Test
     // void updateGameSettings_ExceptionThrown() {
-    //     Mockito.mock(GameSettings.class); 
-    //     Mockito.when(lobbyRepository.findByLobbyId(testLobby.getLobbyId())).thenThrow(new RuntimeException());
-    //     assertThrows(RuntimeException.class, () -> lobbyService.updateGameSettings(testLobby.getLobbyId(), new GameSettings()));
-    //     Mockito.verify(log).info("Something went wrong while updating settings");
+    // Mockito.mock(GameSettings.class);
+    // Mockito.when(lobbyRepository.findByLobbyId(testLobby.getLobbyId())).thenThrow(new
+    // RuntimeException());
+    // assertThrows(RuntimeException.class, () ->
+    // lobbyService.updateGameSettings(testLobby.getLobbyId(), new GameSettings()));
+    // Mockito.verify(log).info("Something went wrong while updating settings");
     // }
 
+    // test for veryfing how lobby is reset
+    @Test
+    void reset_Lobby_success() {
+        testLobby.setWinnerSide(WinnerSide.WEREWOLVES);
+
+        Mockito.when(lobbyRepository.findByLobbyId(testLobby.getLobbyId())).thenReturn(testLobby);
+        Mockito.when(repositoryProvider.getLobbyRepository()).thenReturn(lobbyRepository);
+
+        lobbyService.resetLobby(testLobby.getLobbyId());
+
+        ArgumentCaptor<Lobby> lobbyCaptor = ArgumentCaptor.forClass(Lobby.class);
+        Mockito.verify(lobbyRepository).save(lobbyCaptor.capture());
+        Mockito.verify(lobbyRepository).flush();
+
+        Lobby capturedLobby = lobbyCaptor.getValue();
+        assertEquals(WinnerSide.NOWINNER, capturedLobby.getWinnerSide());
+    }
+
+    // test that checks if lobby is full -> throws error
+    // fix test
+    @Test
+    void givenLobby_lobby_is_full_throws_error() {
+        List<Player> players = createSamplePlayers(4);
+        Mockito.when(repositoryProvider.getPlayerRepository()).thenReturn(playerRepository);
+
+        Mockito.when(lobbyRepository.findByLobbyCode(testLobby.getLobbyCode())).thenReturn(testLobby);
+        Mockito.when(lobbyService.getListOfLobbyPlayers(testLobby.getLobbyCode())).thenReturn(players);
+
+        assertThrows(ResponseStatusException.class, () -> lobbyService.checkIfLobbyFull(testLobby.getLobbyCode()),
+                "The lobby is full. Therefore, the player could not be added!");
+    }
+
+    private List<Player> createSamplePlayers(int count) {
+        List<Player> players = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            Player player = new Player();
+            player.setUsername("testPlayer" + i);
+            player.setLobbyCode("HHHHH");
+        }
+        return players;
+    }
+
+    // test to setDefaultSettings -> less that minnumber of players -> throws an
+    // error
+    @Test
+    void setDefaultSettings_not_enough_players_throws_error() {
+        testLobby.setNumberOfPlayers(3);
+
+        assertThrows(NullPointerException.class, () -> {
+            GameSettings settings = lobbyService.setDefaultSettings(testLobby);
+        }, "Lobby needs more Players");
+    }
 }
